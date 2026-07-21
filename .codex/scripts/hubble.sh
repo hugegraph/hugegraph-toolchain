1e257e9635c981b6f034064ba7e955e35e1effbb
imbajin <jin@apache.org>
2026-07-22 00:01:17 +0800

    fix: make Codex HStore profile ready for local testing

--- .codex/scripts/hubble.sh
+++ .codex/scripts/hubble.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -euo pipefail
 unset CDPATH
 
@@ -444,7 +461,6 @@ show_status() {
 case "${1:-help}" in
     fe-port) fe_port "${2:-${WORKTREE}}" ;;
     be-runtime) echo "${BE_RUNTIME_DIR}" ;;
-    _lock-hold) with_state_lock sleep "${2:-1}" ;;
     fe-start) start_fe ;;
     be-start) start_be false ;;
     be-switch) start_be true ;;
